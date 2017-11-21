1eb1fe981209c2a36e032dd826d1d8373ad69f42
Xilin Sun <s.sn.giraffe@gmail.com>
2017-11-20 18:50:14 -0800

    update alt.js: update comments

--- src/alt.js
+++ src/alt.js
@@ -1,6 +1,7 @@
 // travelx - react and alt.js app showing travel destinations
 
-// require alt: a flux compliant library for JavaScript applications
+// alt.js - require alt, a flux compliant library for JavaScript applications
+
 let Alt = require('alt');
 let alt = new Alt();
 
